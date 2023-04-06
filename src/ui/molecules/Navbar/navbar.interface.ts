5f1fed021fd08c845b929965af346463ce91ccf7
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-04-06 17:24:21 -0300

    feat(Navbar): creation of navbar

---
+++ src/ui/molecules/Navbar/navbar.interface.ts
@@ -0,0 +1,14 @@
+type PossibleNavLinks = "HOME" | "FEEDBACKS" | "BLOG" | "TIMELINE" | "RESUME";
+
+interface NavData {
+  id: PossibleNavLinks;
+  text: string;
+  href: string;
+}
+
+interface Props {
+  selectedLink: PossibleNavLinks;
+}
+
+export default Props;
+export type { NavData };
