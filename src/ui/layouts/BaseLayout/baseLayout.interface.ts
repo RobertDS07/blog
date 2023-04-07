fee0574af7323da7acd2b45e69a15c32c3e72fc8
Robert Damaceno Santos <robert.santos@triider.com>
2023-04-07 19:42:32 -0300

    feat(BaseLayout): finishing BaseLayout

---
+++ src/ui/layouts/BaseLayout/baseLayout.interface.ts
@@ -0,0 +1,9 @@
+import type { PossibleNavLinks } from "@ui/molecules/Navbar/navbar.interface";
+
+interface Props {
+  pageTitle: string;
+  selectedNavLink: PossibleNavLinks;
+  hideOverflow?: boolean;
+}
+
+export default Props;
