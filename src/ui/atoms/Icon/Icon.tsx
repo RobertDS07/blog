c5b14d3a3b80994fcee54c48269a499041d3afc0
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-06-28 01:38:03 -0300

    feat(Icon): re-creating Icon in .tsx to use with solid

---
+++ src/ui/atoms/Icon/Icon.tsx
@@ -0,0 +1,46 @@
+import BlogIcon from "../../../../public/icons/blog-icon.svg";
+import ClockIcon from "../../../../public/icons/clock.svg";
+import EyeIcon from "../../../../public/icons/eye.svg";
+import GithubIcon from "../../../../public/icons/github.svg";
+import HeartIcon from "../../../../public/icons/heart.svg";
+import JobIcon from "../../../../public/icons/job.svg";
+import LinkedinIcon from "../../../../public/icons/linkedin.svg";
+import MenuIcon from "../../../../public/icons/menu.svg";
+import PaperIcon from "../../../../public/icons/paper.svg";
+import TwitterIcon from "../../../../public/icons/twitter.svg";
+import type Props from "./icon.interface";
+
+const Icon = ({ type, ...svgAttributes }: Props) => {
+  const getIcon = () => {
+    switch (type) {
+      case "clock":
+        return ClockIcon;
+      case "job":
+        return JobIcon;
+      case "eye":
+        return EyeIcon;
+      case "heart":
+        return HeartIcon;
+      case "paper":
+        return PaperIcon;
+      case "github":
+        return GithubIcon;
+      case "linkedin":
+        return LinkedinIcon;
+      case "twitter":
+        return TwitterIcon;
+      case "blogIcon":
+        return BlogIcon;
+      case "menuIcon":
+        return MenuIcon;
+      default:
+        throw new Error("Icon not found for type: " + type);
+    }
+  };
+
+  const Icon = getIcon();
+
+  return <Icon {...svgAttributes} />;
+};
+
+export default Icon;
