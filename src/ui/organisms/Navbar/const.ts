3dffc94d5c4aaf94503d31e78f089a1e895b4608
Robert Damaceno Santos <robert.santos@triider.com>
2023-06-28 01:44:10 -0300

    feat(Navbar): passing navbar to /organisms && passing navbar to .tsx to use solid

---
+++ src/ui/organisms/Navbar/const.ts
@@ -0,0 +1,22 @@
+import type { NavData } from "./navbar.interface";
+
+export const NAV_DATA: NavData[] = [
+  {
+    id: "HOME",
+    href: "/",
+    text: "Home",
+  },
+  {
+    id: "FEEDBACKS",
+    href: "/feedbacks",
+    text: "Feedbacks",
+  },
+  {
+    id: "BLOG",
+    href: "/blog",
+    text: "Blog",
+  },
+];
+
+export const RESUME_LINK =
+  "https://drive.google.com/file/d/1L1IrUSl_2byc9kutqT0xaCfrFReqiXap/view?usp=sharing";
