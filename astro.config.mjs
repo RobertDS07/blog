15e52e78c3b57b772f8c7f10214509f82fbf5d6a
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-06-28 01:34:04 -0300

    config(astro.config.mjs): add plugin to help to handle with svg imports && add some integration as: solid, mdx, createOgImage (internal integration) and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,33 @@
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import mdx from "@astrojs/mdx";
+import solid from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import SVGInjectPlugin from "vite-plugin-svg-inject";
+import createOgImage from "./astro-integrations/create-og-image/create-og-image";
 
-// https://astro.build/config
+const { PUCLIC_BASE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
+const NON_INDEXED_PATHS = ["/og/"];
+
 export default defineConfig({
+  site: PUCLIC_BASE_URL,
+  vite: {
+    plugins: [SVGInjectPlugin()],
+  },
   integrations: [
+    solid(),
+    mdx(),
+    createOgImage(),
+    sitemap({
+      filter: (url) => {
+        const isToExclude = NON_INDEXED_PATHS.some((path) =>
+          url.endsWith(path)
+        );
+        return !isToExclude;
+      },
+    }),
     tailwind({
       config: {
         applyBaseStyles: false,
