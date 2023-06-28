d9c334735a6c4055bb52ce56d15b79329acbc5f3
Robert Damaceno Santos <robert.santos@triider.com>
2023-06-28 01:49:54 -0300

    feat(HomeTemplate): add props to use BaseLayout && handle with responsive design && receiving real data via props

---
+++ src/ui/templates/HomeTemplate/homeTemplate.interface.ts
@@ -0,0 +1,9 @@
+import type ArticleCardProps from "@ui/molecules/ArticleCard/articleCard.interface";
+import type FeedbackCardProps from "@ui/molecules/FeedbackCard/feedbackCard.interface";
+
+interface Props {
+  articles: ArticleCardProps[];
+  feedbacks: FeedbackCardProps[];
+}
+
+export default Props;
