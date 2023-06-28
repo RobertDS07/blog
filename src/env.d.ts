39250c0029f60102bff95d539d3838774a4ed053
Robert Damaceno Santos <robert.santos@triider.com>
2023-06-28 01:35:44 -0300

    chore(intelisense): creating type to be used in import.meta.env.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
+/* eslint-disable no-unused-vars */
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="./@types/svg.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  PUCLIC_BASE_URL: string;
+  PUCLIC_MY_TWITTER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
