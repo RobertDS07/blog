0f937b834f9dd03580c92c2d2958b2e07cff905b
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-07-17 16:30:47 -0300

    feat(About): creation of "about" page, only mobile layout for a while

---
+++ src/ui/templates/AboutTemplate/const.ts
@@ -0,0 +1,22 @@
+const WORK_CONTENT = [
+  {
+    startedAt: new Date("2022-02-01"),
+    exitedAt: new Date("2023-02-01"),
+    companyName: "Juntos Somos Mais",
+    position: "Full Stack Developer",
+    technologiesUsed: ["React JS", "React Native", "Nest JS", "Node JS"],
+    overview:
+      "Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.",
+  },
+];
+
+const EXPERIENCES_CONTENT = [
+  {
+    projectName: "Timenote",
+    technologiesUsed: ["TypeScript", "JavaScript", "ReactJS", "Material-UI"],
+    overview:
+      "Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.",
+  },
+];
+
+export { WORK_CONTENT, EXPERIENCES_CONTENT };
