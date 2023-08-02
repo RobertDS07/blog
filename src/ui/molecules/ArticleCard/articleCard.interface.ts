26942457f09fa2c65726552384d5e3182c883986
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-08-02 15:04:27 -0300

    feat(ArticleCard): add new infos of post and add flag to represent tags of post

--- src/ui/molecules/ArticleCard/articleCard.interface.ts
+++ src/ui/molecules/ArticleCard/articleCard.interface.ts
@@ -1,7 +1,12 @@
+import type { CollectionEntry } from "astro:content";
+
 interface Props {
   title: string;
   summary: string;
   articleSlug: string;
+  minutesToRead: number;
+  createdAt: string;
+  tags: CollectionEntry<"posts">["data"]["tags"];
 }
 
 export default Props;
