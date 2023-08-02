5bd889ad2116f6a1eb3157a51d24cafce282b82d
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-08-02 15:03:26 -0300

    feat(blogPost.schema.ts): add enum of possible tags to map all tags of posts

--- src/content/_schemas/blogPost.schema.ts
+++ src/content/_schemas/blogPost.schema.ts
@@ -4,10 +4,10 @@ const blogPostSchema = z.object({
   title: z.string(),
   imageUrl: z.string(),
   description: z.string(),
-  tags: z.array(z.string()),
+  tags: z.array(z.enum(["node", "git", "react"])),
   timeToReadInMinutes: z.number(),
   relatedPosts: z.array(reference("posts")).optional(),
   createdAt: z.string(),
 });
 
-export default blogPostSchema
+export default blogPostSchema;
