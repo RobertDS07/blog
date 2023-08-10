f1cdc6bc774af3201133cb86c17825cf821d2299
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-08-10 14:28:27 -0300

    feat(FeedbackCard): add 3 new props: 'activeShowMoreButton', 'isToTruncate'  and id

--- src/ui/molecules/FeedbackCard/feedbackCard.interface.ts
+++ src/ui/molecules/FeedbackCard/feedbackCard.interface.ts
@@ -8,6 +8,9 @@ interface Props {
   feedback: string;
   photoSrc: string;
   socialMediaData: SocialMediaData;
+  id?: string;
+  isToTruncate?: boolean;
+  activeShowMoreButton?: boolean;
 }
 
 export default Props;
